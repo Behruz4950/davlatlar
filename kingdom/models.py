0056ad22b5a85ab3e19bf76c6897937287597b2e
leaderman0114 <alwaysleader0114@gmail.com>
2021-06-17 14:29:44 +0500

    create.html faylida valueda title o'rniga text berilishi kerak edi xato to'g'irlandi, proyekt ishladi, THE END

--- kingdom/models.py
+++ kingdom/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
-class davlatlar(models.Model):
+class Davlatlar(models.Model):
     text = models.TextField()
--- kingdom/views.py
+++ kingdom/views.py
@@ -1,10 +1,43 @@
-# from django.shortcuts import render
-from kingdom import models
+from typing import Text
+from django.db import models
+from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from kingdom.models import davlatlar
+from .models import Davlatlar
+from django.shortcuts import redirect
+
+def home(request):
+    context = {
+        'posts': Davlatlar.objects.all(),
+    }
+    return render(request,"home.html",context)
 
 
-class IndexView(ListView):
-    model = davlatlar
-    template_name = 'index.html'
-    context_object_name = 'davlatlar'
+def create(request):
+    if request.method == 'POST':
+        p = Davlatlar(text=request.POST['title'])
+        p.save()
+        return redirect('home')
+    
+    return render(request,'create.html')
+
+
+def update(request,id):
+    try:
+        p = Davlatlar.objects.get(id=id)
+    except Davlatlar.DoesNotExist:
+        return redirect('home')
+
+    if request.method == 'POST':
+        p.text = request.POST['title']
+        p.save()
+
+        return redirect('home')
+
+    return render(request,'create.html',{
+        'p':p
+    })
+
+
+def delete(request,id):
+    Davlatlar.objects.filter(id=id).delete()
+    return redirect('home')
